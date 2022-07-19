dab52b493cc1a9d9283bf863bab6e41a4bc1e9c1
WebbD777 <109418123+WebbD777@users.noreply.github.com>
2022-07-19 20:10:32 +0200

    Finished decision scene and controller

--- JDBC_database/src/main/java/com/webbd777/jdbc_database/DecisionController.java
+++ JDBC_database/src/main/java/com/webbd777/jdbc_database/DecisionController.java
@@ -1,22 +1,64 @@
 package com.webbd777.jdbc_database;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.sql.Statement;
+import java.util.concurrent.TimeUnit;
 
 public class DecisionController {
 
     private Statement statement;
 
-    public void Select(ActionEvent event) throws Exception {
+    @FXML
+    private AnchorPane scenePane;
+    @FXML
+    private Parent par;
 
+
+    private Stage stageExit;
+    private Stage stageClose;
+    private Stage stageQuery;
+    private Scene sceneQuery;
+
+    public void Select(ActionEvent event) throws Exception {
+        System.out.println("Selected something");
     }
 
     public void Insert(ActionEvent event) throws Exception {
 
+        //Exit programme
+        stageClose = (Stage)scenePane.getScene().getWindow();
+        stageClose.close();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("addStudent.fxml"));
+        par = loader.load();
+
+        StudentController stuCintol = loader.getController();
+        stuCintol.State(statement);
+
+        stageQuery = (Stage)((Node)event.getSource()).getScene().getWindow();
+        sceneQuery = new Scene(par);
+        stageQuery.setScene(sceneQuery);
+        stageQuery.show();
     }
 
     public void Exit(ActionEvent event) throws Exception {
+
+        stageExit = (Stage)scenePane.getScene().getWindow();
+        stageExit.close();
+
+        Alert alert = new Alert(Alert.AlertType.NONE);
+        alert.setTitle("Excited");
+        TimeUnit.SECONDS.sleep(1);
+        alert.close();
 
     }
 
